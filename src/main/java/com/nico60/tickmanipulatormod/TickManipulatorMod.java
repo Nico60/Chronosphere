59d09ccdd8da9c676f00a0a9c7740b593b6489fe
Nico60 <ndhouailly@gmail.com>
2025-02-17 19:56:31 +0100

    Remove PastSelfEntity for now

--- src/main/java/com/nico60/tickmanipulatormod/TickManipulatorMod.java
+++ src/main/java/com/nico60/tickmanipulatormod/TickManipulatorMod.java
@@ -4,18 +4,11 @@ import com.mojang.logging.LogUtils;
 import com.nico60.tickmanipulatormod.item.BlockRestorerItem;
 import com.nico60.tickmanipulatormod.item.DestinyClockItem;
 import com.nico60.tickmanipulatormod.item.RewindingHourglassItem;
-import com.nico60.tickmanipulatormod.item.TimeCrystalItem;
-import com.nico60.tickmanipulatormod.entity.PastSelfEntity;
 import net.minecraft.client.Minecraft;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.network.chat.Component;
-import net.minecraft.resources.ResourceKey;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.commands.TickCommand;
-import net.minecraft.world.entity.EntityType;
-import net.minecraft.world.entity.MobCategory;
 import net.minecraft.world.item.*;
-import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
@@ -39,16 +32,8 @@ public class TickManipulatorMod {
     public static final String MOD_ID = "tickmanipulatormod";
     private static final Logger LOGGER = LogUtils.getLogger();
     public static final DeferredRegister<CreativeModeTab> MOD_TABS = DeferredRegister.create(Registries.CREATIVE_MODE_TAB, MOD_ID);
-    public static final DeferredRegister<EntityType<?>> ENTITIES = DeferredRegister.create(Registries.ENTITY_TYPE, MOD_ID);
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MOD_ID);
 
-    public static final RegistryObject<EntityType<PastSelfEntity>> PAST_SELF = ENTITIES.register("past_self",
-            () -> EntityType.Builder.of((EntityType<PastSelfEntity> entityType, Level level) -> new PastSelfEntity(entityType, level), MobCategory.MISC)
-                    .sized(0.6F, 1.8F)
-                    .sized(0.6F, 1.8F)
-                    .clientTrackingRange(32)
-                    .build(ResourceKey.create(Registries.ENTITY_TYPE,
-                            ResourceLocation.fromNamespaceAndPath(TickManipulatorMod.MOD_ID,"past_self"))));
 
     public static final RegistryObject<Item> BLOCK_RESTORER = ITEMS.register("block_restorer",
             () -> new BlockRestorerItem(new Item.Properties().stacksTo(1).setId(ITEMS.key("block_restorer"))));
@@ -56,15 +41,12 @@ public class TickManipulatorMod {
             () -> new DestinyClockItem(new Item.Properties().stacksTo(1).setId(ITEMS.key("destiny_clock"))));
     public static final RegistryObject<Item> REWINDING_HOURGLASS = ITEMS.register("rewinding_hourglass",
             () -> new RewindingHourglassItem(new Item.Properties().stacksTo(1).setId(ITEMS.key("rewinding_hourglass"))));
-    public static final RegistryObject<Item> TIME_CRYSTAL = ITEMS.register("time_crystal",
-            () -> new TimeCrystalItem(new Item.Properties().stacksTo(1).setId(ITEMS.key("time_crystal"))));
 
     public static final RegistryObject<CreativeModeTab> MOD_TAB = MOD_TABS.register("mod_tab",
             () -> CreativeModeTab.builder()
                     .icon(() -> new ItemStack(DESTINY_CLOCK.get()))
                     .title(Component.translatable("itemGroup.tickmanipulatormod"))
                     .displayItems((parameters, output) -> {
-                        output.accept(TIME_CRYSTAL.get());
                         output.accept(DESTINY_CLOCK.get());
                         output.accept(REWINDING_HOURGLASS.get());
                         output.accept(BLOCK_RESTORER.get());
@@ -74,7 +56,6 @@ public class TickManipulatorMod {
         IEventBus modEventBus = context.getModEventBus();
         modEventBus.addListener(this::commonSetup);
         MOD_TABS.register(modEventBus);
-        ENTITIES.register(modEventBus);
         ITEMS.register(modEventBus);
         MinecraftForge.EVENT_BUS.register(this);
         modEventBus.addListener(this::addCreative);
